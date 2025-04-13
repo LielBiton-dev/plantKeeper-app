5cd2413cf15a57379e23e0a98f98fe120fc9eac1
LielBiton-dev <156704194+LielBiton-dev@users.noreply.github.com>
2025-04-13 16:34:55 +0300

    Delete src directory

--- src/components/PageTransition.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./PageTransition.css";
-
-const PageTransition = ({ children }) => {
-  return <div className="page-fade">{children}</div>;
-};
-
-export default PageTransition;
